f94668c7d8e2c10a673de18b1a6db6dd9265ead8
emonovaev <lena.jemonova@gmail.com>
2024-01-28 19:07:12 +0300

    add raifTest

---
+++ src/test/java/ProjectTests.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+
+public class ProjectTests  extends TestBase{
+
+    @Test
+    @DisplayName("Проверка разделов в меню")
+    void checkMenuContentTest(){
+        open("https://www.raiffeisen.ru/");
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Кредиты"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Ипотека"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Карты"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Сбережения"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Инвестиции"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Страхование"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Переводы"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Сервисы"));
+        $("[data-marker='MainMenu.MainMenuWrapper']").shouldHave(text("Про Онлайн-банк"));
+    }
+
+@Test
+@DisplayName("Проверка ссылок на app Store")
+    void checkLinkToAppTest(){
+        open("https://www.raiffeisen.ru/");
+        $("[data-marker='MobileApp.Box']").$$("[data-marker='MobileApp.Button']").shouldHave(size(3));
+    }
+
+
+    @Test
+    @DisplayName("Проверка открытия чата")
+
+    void checkOpenChatTest(){
+        open("https://www.raiffeisen.ru/");
+        $("[data-marker='Chat.Button']").click();
+        $("[data-testid='rchat-root-window']").should(appear);
+    }
+
+    @Test
+    @DisplayName("Проверка наличия кнопки Подать заявку в разделе Ипотека")
+
+    void checkIpotekaButton(){
+        open("https://www.raiffeisen.ru/");
+        $("[data-marker='MainMenu.MainMenuWrapper']").$(byText("Ипотека")).click();
+        $("[data-marker='MainMenu.div_3']").$(byText("Вторичный рынок")).click();
+        $("[data-marker='Product.Button']").shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Проверка наличия вакансий IT и DGTL")
+
+    void checkCareerChapterTest(){
+        open("https://www.raiffeisen.ru/");
+        $("[data-marker='LinkList.LinkListNav']").$(byText("О банке")).click();
+        $("[data-marker='StoryIconBlockItem.Grid.Col']", 1).click();
+        $(".tabs-0-2-6").$(byText("IT и DGTL")).click();
+        $(".vacancies-0-2-95").$$("[target='_blank']").shouldHave(size(0));
+    }
+
+
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @AfterEach
+    void clearAll() {
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
+        closeWebDriver();
+    }
+}
