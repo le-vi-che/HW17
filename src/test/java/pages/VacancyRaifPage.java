c5c66af7e91542bcb2932781ad648ea0daf87c2b
emonovaev <lena.jemonova@gmail.com>
2024-02-03 14:12:33 +0300

    add files

--- src/test/java/pages/VacancyRaifPage.java
+++ src/test/java/pages/VacancyRaifPage.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
